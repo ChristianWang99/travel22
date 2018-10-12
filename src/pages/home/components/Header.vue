fffeb78e53da80aa7606978d2f9d059c7cc04297
ChristianWang99 <upteep@163.com>
2018-10-12 11:57:32 +0800

    add home header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+      左
+    </div>
+    <div class="header_input">
+      中
+    </div>
+    <div class="header_right">
+      右
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+.header{
+  height: .88rem;
+  line-height: .88rem;
+  background-color: red;
+  display: flex;
+}
+.header_left{
+  float: left;
+  width: .60rem;
+}
+.header_input{
+  flex: 1;
+  background-color: #ffffff;
+  border-radius: 3px;
+  height: .64rem;
+  line-height: .64rem;
+  margin-top: .12rem;
+}
+.header_right{
+  float: right;
+}
+</style>
